fd0530ba3844fbd1c982716c926136c97dcc66fb
basyura <basyura@gmail.com>
2019-04-08 17:59:16 +0900

    initial

---
+++ content_scripts/content.js
@@ -0,0 +1,16 @@
+
+var date = new Date();
+
+window.onload = function() {
+
+  setInterval(function() {
+      var diff = (new Date().getTime()) - date.getTime();
+      if (diff > 5 * 60 * 1000) {
+        location.reload()
+      }
+  }, 60 * 1000);
+};
+
+window.onmousemove = function() {
+  date = new Date();
+};
